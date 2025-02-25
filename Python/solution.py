89c1fa55d9725ab5a24dce15b9ca18c4827110a3
Shazeel khan <75373648+shazeel-hub@users.noreply.github.com>
2025-02-25 18:48:12 +0500

    Add files via upload

---
+++ Python/solution.py
@@ -0,0 +1,57 @@
+# Age Checkerpython
+
+
+age = 60 
+if age < 13 :
+    print ("child")
+
+elif age < 20 :
+    print ("teenager")
+
+elif age < 60 :
+    print ("adult")
+else :
+    print ("senior")
+
+# Price of ticket and dissount
+
+age = 17
+day = "wednesday" or "tuesday"
+
+price = 12 if age >= 18 else 8
+if day == "tuesdy" or "wednesday":
+    price -= 2
+
+print ("Here is Your some special Disscount $",price)
+
+
+# student grades Depends on marks
+
+Marks = 50 
+
+if Marks >= 90 :
+    Grade = "A1"
+elif Marks >= 80:
+    Grade = "A+"
+elif Marks >= 70:
+    Grade = "A"
+elif Marks >= 60:
+    Grade = "b"
+elif Marks >= 50:
+    Grade = "c"
+else :
+    Grade = "Fail"
+
+
+if Grade == "A1":
+    print("exellent work shoaib")
+elif Grade == "A+":
+    print ("very Very Good Work Sohail")
+elif Grade == "A":
+    print("good work Ali")
+elif Grade == "B":
+    print("Do Hard Work")
+elif Grade == "c" :
+    print("Do More Hard Work")
+else :
+    print("Fail")
